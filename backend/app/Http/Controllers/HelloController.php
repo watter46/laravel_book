bb91183c0757a2aa996e38d3b5fc9048abec0bea
watter46 <w.aoki.14@gmail.com>
2021-11-25 16:22:24 +0900

    p.130 finished

---
+++ backend/app/Http/Controllers/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class HelloController extends Controller
+{
+    public function index($id) {
+        $ids = explode(',', $id);
+        $msg = 'get people.';
+        $result = DB::table('people')
+                ->whereBetween('id', $ids)
+                ->get();
+
+        $data = [
+            'msg' => $msg,
+            'data'=> $result,
+        ];
+        return view('hello.index', $data);
+    }
+}
