594c9b9d010e33ef6e5304fb29244471ff24f8e6
watter46 <w.aoki.14@gmail.com>
2021-11-28 17:27:55 +0900

    p.146 finished

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -9,9 +9,8 @@ use App\Person;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $id = $request->query('page');
-        $msg = 'show page: '. $id;
-        $result = Person::paginate(3);
+        $msg = 'show people recorid.';
+        $result = Person::get();
 
         $data = [
             'msg' => $msg,
