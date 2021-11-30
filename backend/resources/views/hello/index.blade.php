224a98a59fb8cce17555ec455ceace456c3c3eaa
watter46 <w.aoki.14@gmail.com>
2021-11-30 14:40:08 +0900

    p.167 finished

--- backend/resources/views/hello/index.blade.php
+++ backend/resources/views/hello/index.blade.php
@@ -6,23 +6,56 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Document</title>
+
+    <script>
+    function doAction(){
+        let id = document.querySelector('#id').value;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', '/hello/json/' + id, true);
+        xhr.responseType = 'json';
+        xhr.onload = function(e) {
+            if (this.status == 200) {
+                let result = this.response;
+                document.querySelector('#name').textContent
+                    = result.name;
+                document.querySelector('#mail').textContent
+                    = result.mail;
+                document.querySelector('#age').textContent
+                    = result.age;
+            }
+        };
+        xhr.send();
+    }
+    </script>
 </head>
 <body>
-    <p>{{ $msg }}</p>
-        <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">id</th>
-                <th scope="col">name and age</th>
-            </thead>
-            <tbody>
-            @foreach ($data as $item)
-                <tr>
-                <th scope="row">{{ $item->id }}</th>
-                <td>{{ $item->name_and_age }}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+    <h1>Hello/Index</h1>
+    {{-- <p>{{ $msg }}</p> --}}
+    <div>
+        <input type="number" id="id" value="1">
+        <button onclick="doAction();">Click</button>
+    </div>
+
+    <ul>
+        <li id="name"></li>
+        <li id="mail"></li>
+        <li id="age"></li>
+    </ul>
+
+    {{-- <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">id</th>
+            <th scope="col">name and age</th>
+        </thead>
+        <tbody>
+        @foreach ($data as $item)
+            <tr>
+            <th scope="row">{{ $item->id }}</th>
+            <td>{{ $item->name_and_age }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table> --}}
 </body>
 </html>
