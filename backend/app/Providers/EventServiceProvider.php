13b742bab2f1245d265b2cd397e382f878b5a5d9
watter46 <w.aoki.14@gmail.com>
2021-12-02 02:38:58 +0900

    p.210 finished

--- backend/app/Providers/EventServiceProvider.php
+++ backend/app/Providers/EventServiceProvider.php
@@ -9,19 +9,16 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
+    public function shouldDiscoverEvents()
+    {
+        return true;
+    }
     /**
      * The event listener mappings for the application.
      *
      * @var array
      */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-        'App\Events\PersonEvent' => [
-            'App\Listeners\PersonEventListener',
-        ],
-    ];
+
 
     /**
      * Register any events for your application.
